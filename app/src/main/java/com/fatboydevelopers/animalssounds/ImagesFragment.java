c19bcba766f2fe52c92def74113cd46292622c83
Meir <Meirtz4@gmail.com>
2015-10-21 21:26:03 +0300

    Clean some code

--- app/src/main/java/com/fatboydevelopers/animalssounds/ImagesFragment.java
+++ app/src/main/java/com/fatboydevelopers/animalssounds/ImagesFragment.java
@@ -1,27 +1,18 @@
 package com.fatboydevelopers.animalssounds;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by Meir on 10/20/2015.
  */
 public class ImagesFragment extends android.support.v4.app.Fragment{
-    // Store instance variables
-    private String title;
-    private int page;
 
     // newInstance constructor for creating fragment with arguments
     public static ImagesFragment newInstance(int page, String title) {
         ImagesFragment fragmentFirst = new ImagesFragment();
-        Bundle args = new Bundle();
-        args.putInt("someInt", page);
-        args.putString("someTitle", title);
-        fragmentFirst.setArguments(args);
         return fragmentFirst;
     }
 
@@ -29,8 +20,6 @@ public class ImagesFragment extends android.support.v4.app.Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt", 0);
-        title = getArguments().getString("someTitle");
     }
 
     // Inflate the view for the fragment based on layout XML
@@ -38,8 +27,6 @@ public class ImagesFragment extends android.support.v4.app.Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.images_layout, container, false);
-//        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
-//        tvLabel.setText(page + " -- " + title);
         return view;
     }
 }
--- app/src/main/java/com/fatboydevelopers/animalssounds/MenuFragment.java
+++ app/src/main/java/com/fatboydevelopers/animalssounds/MenuFragment.java
@@ -9,17 +9,10 @@ import android.view.ViewGroup;
  * Created by Meir on 10/20/2015.
  */
 public class MenuFragment extends android.support.v4.app.Fragment{
-    // Store instance variables
-    private String title;
-    private int page;
 
     // newInstance constructor for creating fragment with arguments
     public static MenuFragment newInstance(int page, String title) {
         MenuFragment fragmentFirst = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putInt("someInt", page);
-        args.putString("someTitle", title);
-        fragmentFirst.setArguments(args);
         return fragmentFirst;
     }
 
@@ -27,8 +20,6 @@ public class MenuFragment extends android.support.v4.app.Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("someInt", 0);
-        title = getArguments().getString("someTitle");
     }
 
     // Inflate the view for the fragment based on layout XML
@@ -36,8 +27,6 @@ public class MenuFragment extends android.support.v4.app.Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu_layout, container, false);
-//        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
-//        tvLabel.setText(page + " -- " + title);
         return view;
     }
 }
